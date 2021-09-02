5379c761e0be0893ce77cba307b85b66163d0b96
Peter Kuzyk <p.kuzyk@yahoo.com>
2021-09-01 22:32:09 -0500

    User is able to see Maggie May on a first page

--- support/pageobjects/home.page.js
+++ support/pageobjects/home.page.js
@@ -1,47 +1,67 @@
 import Page from "./page"
-import { Component } from "../components"
+import {Component, Pagination} from "../components"
+
 
 export default class HomePage extends Page {
-  constructor() {
-    super()
-    this._homePagePuppySection = new Component({ selector: "#content" })
-    this._puppyName = new Component ({ selector: ".puppy_list h3" })
-    this._viewDetailsButton = new Component ({ selector: ".puppy_list input" })
-  }
-
-  /**
-   * Checks that the home page is displayed
-   */
-  isOpen() {
-    this._homePagePuppySection.isDisplayed()
-  }
-
-  open() {
-    return super.open("")
-  }
-
-  /**
-   * Returns the index of a puppy
-   * @param name
-   * @returns {number}
-   */
-  findPuppyIndex(name) {
-    this._puppyName.waitForDisplayed()
-    let puppyIndex
-    for(let i = 0; i < this._puppyName.elements.length; i++) {
-      if (this._puppyName.elements[i].getText() === name) {
-        puppyIndex = i
-      }
+    constructor() {
+        super()
+        this._homePagePuppySection = new Component({selector: "#content"})
+        this._puppyName = new Component({selector: ".puppy_list h3"})
+        this._viewDetailsButton = new Component({selector: ".puppy_list input"})
+        this._activeSelector = new Pagination({selector: "[class*='current']"})
     }
-    return puppyIndex
-  }
 
-  /**
-   * Proceeds to the detail section for a specific puppy
-   * @param name
-   */
-  proceedToViewDetailsForPuppy(name) {
-    let index = this.findPuppyIndex(name)
-    this._viewDetailsButton.elements[index].click()
-  }
+    /**
+     * Checks that the home page is displayed
+     */
+    isOpen() {
+        this._homePagePuppySection.isDisplayed()
+    }
+
+    open() {
+        return super.open("")
+    }
+
+    /**
+     * Returns the index of a puppy
+     * @param name
+     * @returns {number}
+     */
+    findPuppyIndex(name) {
+        this._puppyName.waitForDisplayed()
+        let puppyIndex
+        for (let i = 0; i < this._puppyName.elements.length; i++) {
+            if (this._puppyName.elements[i].getText() === name) {
+                puppyIndex = i
+            }
+        }
+        return puppyIndex
+    }
+
+    /**
+     * Proceeds to the detail section for a specific puppy
+     * @param name
+     */
+    proceedToViewDetailsForPuppy(name) {
+        let index = this.findPuppyIndex(name)
+        this._viewDetailsButton.elements[index].click()
+    }
+
+    /**
+     * Verify if specific puppy is present
+     * @param puppy
+     */
+    verifyPuppyIsPresent(puppy) {
+        let index = this.findPuppyIndex(puppy)
+        this._puppyName.elements[index].isDisplayed()
+    }
+
+    /**
+     * Verify curent page number
+     * @param number
+     */
+    verifyPageNumber(number) {
+        expect(this._activeSelector.getText()).toEqual(number)
+    }
+
 }
